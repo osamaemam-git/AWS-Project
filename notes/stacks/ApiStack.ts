765c31606562fe05f1d83694ea89a985bfc2c13b
Osama EMAM <osamaemam@MBP-POLLOCK.local>
2023-10-11 12:25:36 +0200

    Adding API

---
+++ notes/stacks/ApiStack.ts
@@ -0,0 +1,30 @@
+import { Api, StackContext, use } from "sst/constructs"
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack }: StackContext) {
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        bind: [table],
+      },
+    },
+    routes: {
+      "POST /notes": "packages/functions/src/create.handler",
+      "GET /notes/{id}": "packages/functions/src/get.main",
+      "PUT /notes/{id}": "packages/functions/src/update.main",
+    },
+  });
+
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  // Return the API resource
+  return {
+    api,
+  };
+}
